23e55484364e459a7d6ff3b2900afa8ff3836b01
Hafizh <m.hafizhridho676@gmai.com>
2023-06-07 16:28:24 +0800

    feat(user):login

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,45 @@
 const {User} = require("../db/models");
 const bcrypt = require("bcrypt");
 module.exports = {
-	User, bcrypt
+	login: async (req, res, next) => {
+        try {
+            const {email, password} = req.body;
+
+            const user = await User.findOne({where: {email}});
+            if (!user) {
+                return res.status(404).json({
+                    status: false,
+                    message: 'email or password is not correct!',
+                    data: null
+                });
+            }
+
+            const passwordCorrect = await bcryp.compare(password, user.password);
+            if (!passwordCorrect) {
+                return res.status(404).json({
+                    status: false,
+                    message: 'email or password is not correct!',
+                    data: null
+                });
+            }
+
+            const payload = {
+                id: user.id,
+                name: user.name,
+                email: user.email
+            };
+
+            const token = await jwt.sign(payload, JWT_SECRET_KEY);
+            return res.status(200).json({
+                status: true,
+                message: 'success!',
+                data: {
+                    token: token
+                }
+            });
+
+        } catch (err) {
+            next(err);
+        }
+    }
 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,3 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const { user } = require("../controllers");
+
+router.post("/login", user.login);
+
+
+
+module.exports = router;
 
 
 
