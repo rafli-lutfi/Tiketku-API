c1624fe453465d0b47a6967f25dcfb97a75a2358
Hafizh <m.hafizhridho676@gmai.com>
2023-06-13 09:53:14 +0800

    feat(price):create

--- src/controllers/price.js
+++ src/controllers/price.js
@@ -18,13 +18,35 @@ module.exports = {
 	create: async (req, res, next) => {
 		try {
 			const { flight_id, seat_type, price, discount } = req.body;
+			if(!flight_id || !seat_type || !price || !discount){
+				return res.status(400).json({
+					status: false,
+					message: "missing body request",
+					data: null
+				});
+			}
 
-			const harga = await Price.create({ flight_id, seat_type, price, discount });
-    
+			
+			const checkPrice = await Price.findOne({where: {flight_id}});
+			if (checkPrice){
+				return res.status(400).json({
+					status: false, 
+					message: "price already exist",
+					data: null
+				});
+			}
+            
+			const newPrice = await Price.create({ flight_id, seat_type, price, discount});
+
 			return res.status(201).json({
 				status: true,
 				message: "Price created ",
-				data: harga
+				data: {
+					flight_id: newPrice.flight_id, 
+					seat_type: newPrice.seat_type, 
+					price: newPrice.price, 
+					discount: newPrice.discount 
+				}
 			});
 		} catch (error) {
 			next(error);
