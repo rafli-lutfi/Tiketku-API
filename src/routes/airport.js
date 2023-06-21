394eb7887eb07bbd5c5998e172bb122b73d48118
rafli-lutfi <rafli.lutfi1818@gmail.com>
2023-06-21 10:59:28 +0700

    feat(role): add certain endpoint that can only be accessed by ADMIN

--- src/routes/airport.js
+++ src/routes/airport.js
@@ -1,8 +1,11 @@
 const {airport} = require("../controllers");
 const express = require("express");
 const router = express.Router();
+const role = require("../middlewares/role");
+const jwt = require("../middlewares/jwtAuth");
+
 
 router.get("/", airport.getAll);
-router.post("/", airport.create);
+router.post("/", jwt.authenticate, role.hasRole(["ADMIN"]), airport.create);
 
 module.exports = router;
