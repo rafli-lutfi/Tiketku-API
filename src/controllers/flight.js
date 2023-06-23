9092dfa5eab4c92f9636eb6e28c5bdc2a5691e83
Syaifun Nadhif <nadhifeldrafo@gmail.com>
2023-06-23 19:33:57 +0700

    ft: notif

--- src/controllers/flight.js
+++ src/controllers/flight.js
@@ -251,54 +251,6 @@ module.exports = {
 		}
 	},
 
-	create: async (req, res, next) => {
-		try {
-			const {airplane_id, from_airport_id, to_airport_id, date, departure_time, arrival_time, estimation} = req.body;
-
-			if(!airplane_id || !from_airport_id || !to_airport_id || !date || !departure_time || !arrival_time || !estimation){
-				throw new Error("mising body request");
-			}
-
-			const checkAirplane = await Airplane.findAndCountAll({ where: { id: airplane_id } });
-
-			if (checkAirplane.count != airplane_id.length) {
-				throw new Error("there's airplane_id not found");
-			}
-
-			const checkFromAirport = await Airport.findAndCountAll({ where: { id: from_airport_id} });
-
-			if (checkFromAirport.count != from_airport_id.length) {
-				throw new Error("there's from_airport_id not found");
-			}
-
-			const checkToAirport = await Airport.findAndCountAll({ where: { id: to_airport_id} });
-
-			if (checkToAirport.count != to_airport_id.length) {
-				throw new Error("there's to_airport_id not found");
-			}
-
-			if (to_airport_id == from_airport_id){
-				throw new Error("airports are not the same");
-			}
-
-			const newFlight = await Flight.create({ airplane_id, from_airport_id, to_airport_id, date, departure_time, arrival_time, estimation });
-
-			return res.status(201).json({
-				status: true,
-				message: "success create new flight",
-				data: newFlight,
-			});
-		} catch (err) {
-			if (err.message == "missing body request" || err.message == "there's airplane_id not found" || err.message == "there's from_airport_id not found"  || err.message == "there's to_airport_id not found" || err.message == "airports are not the same") {
-				return res.status(400).json({
-					status: false,
-					message: err.message,
-					data: null,
-				});
-			}
-			next(err);
-		}
-	},
 
 	detail: async(req, res, next) => {
 		try {
