7819b9aed43294c057bd011e0b40d3a731fb0e3d
rafli-lutfi <rafli.lutfi1818@gmail.com>
2023-06-27 23:34:58 +0700

    feat(validation): refactor code

--- src/middlewares/validation/otp.js
+++ src/middlewares/validation/otp.js
@@ -1,72 +1,36 @@
 const Joi = require("joi");
 const {isEmailVerified} = require("./existence");
-const respone = require("../../utils/respone");
-
-const options = {
-	abortEarly: false,
-	allowUnknown: true,
-	stripUnknown: true,
-	errors: {
-		wrap: {
-			label: ""
-		}
-	}
-};
-
 
 module.exports = {
-	resendEmailVerification: async (req, res, next) => {
-		const querySchema = Joi.object({
+	resendEmailVerification: {
+		querySchema : Joi.object({
 			token: Joi.string().required()
-		});
+		}),
 
-		const bodySchema = Joi.object({
+		bodySchema : Joi.object({
 			email: Joi.string().email().required()
 				.external(async (value) => {
 					return await isEmailVerified(value);
 				})
-		});
-
-		try {
-			req.query = await querySchema.validateAsync(req.query, options);
-			req.body = await bodySchema.validateAsync(req.body, options);
-			next();
-		} catch (error) {
-			return respone.errorJoiValidation(res, error);
-		}
+		}),
 	},
 
-	verifyAccount: async (req, res, next) => {
-		const schema = Joi.object({
+	verifyAccount: {
+		querySchema : Joi.object({
 			token: Joi.string().required()
-		});
-
-		try {
-			req.query = await schema.validateAsync(req.query, options);
-			next();
-		} catch (error) {
-			return respone.errorJoiValidation(res, error);
-		}
+		}),
 	},
 
-	resetPassword: async (req, res, next) => {
-		const querySchema = Joi.object({
+	resetPassword: {
+		querySchema : Joi.object({
 			token: Joi.string().required()
-		});
+		}),
 
-		const bodySchema = Joi.object({
+		bodySchema : Joi.object({
 			new_password: Joi.string().min(8).max(30).required(),
 			confirm_new_password: Joi.string().required()
 				.valid(Joi.ref("new_password"))
 				.messages({"any.only": "password not match!"})
-		});
-
-		try {
-			req.query = await querySchema.validateAsync(req.query, options);
-			req.body = await bodySchema.validateAsync(req.body, options);
-			next();
-		} catch (error) {
-			return respone.errorJoiValidation(res, error);
-		}
+		}),
 	},
 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,19 +3,20 @@ const multer = require("multer");
 const router = express.Router();
 const { user, uploadFile, otp, } = require("../controllers");
 const jwt = require("../middlewares/jwtAuth");
-const userValidation = require("../middlewares/validation/user");
-const otpValidation = require("../middlewares/validation/otp");
+const validate = require("../middlewares/validate");
+const {register, forgotPassword, login, updateProfile} = require("../middlewares/validation/user");
+const {resendEmailVerification, resetPassword, verifyAccount} = require("../middlewares/validation/otp");
 
-router.post("/auth/register", userValidation.register, user.register);
-router.post("/auth/login", userValidation.login, user.login);
+router.post("/auth/register", validate(register), user.register);
+router.post("/auth/login", validate(login), user.login);
 router.get("/auth/oauth", user.googleOauth2);
 
-router.post("/register/verifyAccount", otpValidation.verifyAccount, otp.verifyAccount);
-router.post("/auth/resetPassword", otpValidation.resetPassword, otp.resetPassword);
+router.post("/register/verifyAccount", validate(verifyAccount), otp.verifyAccount);
+router.post("/auth/resetPassword", validate(resetPassword), otp.resetPassword);
 
-router.put("/user/updateProfile", multer().single("media"), userValidation.updateProfile, jwt.authenticate,  uploadFile.uploadAvatar, user.updateProfile);
-router.post("/user/forgotpassword", userValidation.forgotPassword, user.forgotPassword);
-router.post("/user/resendVerification", otpValidation.resendEmailVerification, otp.resendEmailVerfication);
+router.put("/user/updateProfile", multer().single("media"), validate(updateProfile), jwt.authenticate,  uploadFile.uploadAvatar, user.updateProfile);
+router.post("/user/forgotpassword", validate(forgotPassword), user.forgotPassword);
+router.post("/user/resendVerification", validate(resendEmailVerification), otp.resendEmailVerfication);
 router.get("/user/getdetail", jwt.authenticate, user.getDetail);
 
 
